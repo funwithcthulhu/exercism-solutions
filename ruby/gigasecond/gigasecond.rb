39ba50cfab2c4e87e90ee86072517c0f7d6664ad
funwithcthulhu <thomasboeding@gmail.com>
2017-09-30 09:25:20 -0500

    gigasecond

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,15 @@
 class Gigasecond
+  GIGASECOND = 1_000_000_000
   def self.from(time)
-    time + 1000000000
+    verify_input(time)
+    time + GIGASECOND
+  end
+
+  def self.verify_input(time)
+    message = 'Please input Time object or moment in seconds'
+    unless time.is_a?(Time) || time.is_a?(Integer)
+      raise ArgumentError.new, message
+    end
   end
 end
 
